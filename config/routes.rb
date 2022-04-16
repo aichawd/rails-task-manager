04ab572911d4492755a367e40df5b47ad02f6093
Aicha Diagne <awdstrategy@gmail.com>
2022-04-16 15:02:50 +0200

    tasks: list, show, create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+  # create a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # Show the information of one specific task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
